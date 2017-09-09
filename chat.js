70c504a1d109bd96d84d5dc7c2837f216ac3baaf
zavolokas <sergey.zavoloka@gmail.com>
2017-09-09 17:12:17 +0200

    fix small issue

--- chat.js
+++ chat.js
@@ -3,18 +3,20 @@ exports.initChat = function (server) {
     var redis = require('redis').createClient();
 
     io.sockets.on('connection', function (client) {
-        redis.lrange('messages', 0, -1, function (err, msgs) {
-            msgs = msgs.reverse();
-            msgs.forEach(function (m) {
-                client.to(client.room).emit('message', m);
-            });
-        });
 
-        client.on('join', function(data){
-            console.log(data);
-            client.nickname = data.name;
-            client.room = data.chat;
+
+        client.on('joinchat', function (data) {
             client.join(data.chat);
+
+            client.nickname = data.name;
+            client.chatroom = data.chat;
+            
+            redis.lrange('messages-' + client.chatroom, 0, -1, function (err, msgs) {
+                msgs = msgs.reverse();
+                msgs.forEach(function (m) {
+                    client.emit('message', m);
+                });
+            });
         });
 
         console.log('user connected');
@@ -24,21 +26,18 @@ exports.initChat = function (server) {
             var nickname = client.nickname;
             msg = nickname + ': ' + msg;
 
-            redis.lpush('messages', msg, function () {
-                redis.ltrim('messages', 0, 3);
+            redis.lpush('messages-' + client.chatroom, msg, function () {
+                redis.ltrim('messages-' + client.chatroom, 0, 9);
             });
-
-            console.log('user said: ' + msg);
-            client.to(client.room).broadcast.emit('message', msg);
-            //client.emit('message', msg);
+            client.broadcast.to(client.chatroom).emit('message', msg);
         });
 
         client.on('typing', function (username) {
-            client.to(client.room).broadcast.emit('typing', username);
+            client.broadcast.to(client.chatroom).emit('typing', username);
         });
 
         client.on('disconnect', function () {
-            console.log('user disconnected');
+            console.log(client.nickname + ' disconnected');
         });
     });
 };
