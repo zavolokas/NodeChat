fcf8dd9e48449ab0df035ab9baeee9667523455e
zavolokas <sergey.zavoloka@gmail.com>
2017-09-09 10:12:32 +0200

    move server chat to a separate file

---
+++ chat.js
@@ -0,0 +1,32 @@
+exports.initChat = function (server) {
+    var io = require('socket.io')(server);
+    var redis = require('redis').createClient();
+
+    io.sockets.on('connection', function (client) {
+        redis.lrange('messages', 0, -1, function (err, msgs) {
+            msgs = msgs.reverse();
+            msgs.forEach(function (m) {
+                client.emit('message', m);
+            });
+        });
+
+        console.log('user connected');
+
+        client.on('message', function (msg) {
+            redis.lpush('messages', msg, function () {
+                redis.ltrim('messages', 0, 3);
+            });
+            console.log('user said: ' + msg);
+            client.broadcast.emit('message', msg);
+            //client.emit('message', msg);
+        });
+
+        client.on('typing', function (username) {
+            client.broadcast.emit('typing', username);
+        });
+
+        client.on('disconnect', function () {
+            console.log('user disconnected');
+        });
+    });
+};
