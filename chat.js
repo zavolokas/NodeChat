ceccdc6de3cf0a0bdd42ee55a100ed90a6d1c29e
zavolokas <sergey.zavoloka@gmail.com>
2018-08-08 18:52:35 +0200

    update

--- chat.js
+++ chat.js
@@ -1,6 +1,8 @@
 exports.initChat = function (server) {
+    var http = require('http');
     var io = require('socket.io')(server);
-    var redis = require('redis').createClient(process.env.REDIS_URL);
+    var redisUrl = 'redis://' + process.env.REDIS_SERVICE_HOST + ':' +process.env.REDIS_SERVICE_PORT;
+    var redis = require('redis').createClient(redisUrl);
 
     io.sockets.on('connection', function (client) {
 
@@ -10,7 +12,7 @@ exports.initChat = function (server) {
 
             client.nickname = data.name;
             client.chatroom = data.chat;
-            
+
             redis.lrange('messages-' + client.chatroom, 0, -1, function (err, msgs) {
                 msgs = msgs.reverse();
                 msgs.forEach(function (m) {
@@ -23,6 +25,32 @@ exports.initChat = function (server) {
 
         client.on('message', function (msg) {
 
+            console.log(redisUrl);
+
+            if (msg == 'error'){
+                throw new Error('Yeblys');
+            }
+
+            var employeeUrl = 'http://' + process.env.EMPLOYEE_SVC_SERVICE_HOST;
+
+            if (msg == 'account') {
+                var options = {
+                    host: employeeUrl,
+                    port: process.env.EMPLOYEE_SVC_SERVICE_PORT,
+                    path: '/employee',
+                    method: 'GET'
+                };
+
+                http.request(options, function (res) {
+                    console.log('STATUS: ' + res.statusCode);
+                    console.log('HEADERS: ' + JSON.stringify(res.headers));
+                    res.setEncoding('utf8');
+                    res.on('data', function (chunk) {
+                        console.log('BODY: ' + chunk);
+                    });
+                }).end();
+            }
+
             var nickname = client.nickname;
             msg = nickname + ': ' + msg;
 
@@ -30,6 +58,8 @@ exports.initChat = function (server) {
                 redis.ltrim('messages-' + client.chatroom, 0, 9);
             });
             client.broadcast.to(client.chatroom).emit('message', msg);
+
+           
         });
 
         client.on('typing', function (username) {
